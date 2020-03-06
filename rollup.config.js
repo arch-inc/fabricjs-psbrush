b458b277a7637ff7c3a006e5835687d991dd49ca
Jun Kato <i@junkato.jp>
2020-03-07 03:02:19 +0900

    Add boilerplate

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
+
+const checkComments = function(_node, comment) {
+  const { value, type } = comment;
+  return type === "comment2" && /@preserve|@license|@cc_on/i.test(value);
+};
+
+export default [
+  {
+    input: "lib/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs"
+      },
+      {
+        file: pkg.module,
+        format: "es"
+      }
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      typescript({
+        typescript: require("typescript")
+      }),
+      terser({
+        output: {
+          comments: checkComments
+        }
+      })
+    ]
+  }
+];
