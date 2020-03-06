d9b23108fa960c55a3df205a58cbaed0da24d540
Jun Kato <i@junkato.jp>
2020-03-07 03:02:34 +0900

    New build

---
+++ dist/index.es.js
@@ -0,0 +1,1 @@
+function o(){console.log("hello world!")}export{o as hello};
---
+++ dist/index.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.hello=function(){console.log("hello world!")};
