3c084fc6ba9cc8106bd2e5c4ae1c731aa20d5d73
Jun Kato <i@junkato.jp>
2020-03-10 05:18:51 +0900

    Update example code

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,22 +1,24 @@
 import { NextPage } from "next";
 import Head from "next/head";
 
-const js = `
-  let canvas = new fabric.Canvas("c");
-  console.log("fabric", fabric);
-`;
-
-const Index: NextPage = () => (<>
-  <Head>
-    <title key="title">fabricjs-psbrush | A lightweight pressure-sensitive brush implementation for Fabric.js</title>
-  </Head>
-  <div>We're working on documentations -- stay tuned!</div>
-  <canvas id="c"></canvas>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/3.6.2/fabric.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@arch-inc/fabricjs-psbrush@0.0.9/dist/index.js"></script>
-  <script dangerouslySetInnerHTML={{
-    __html: js
-  }}></script>
-</>);
+const Index: NextPage = () => {
+  return (<>
+    <Head>
+      <title key="title">fabricjs-psbrush | A lightweight pressure-sensitive brush implementation for Fabric.js</title>
+      <style>{`
+        canvas {
+          border: 1px solid #ccc;
+        }
+      `}</style>
+    </Head>
+    <p>We're working on documentations -- stay tuned!</p>
+    <div dangerouslySetInnerHTML={{
+      __html: "<canvas id=\"c\" width=\"720\" height=\"480\" />"
+    }}></div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/3.6.2/fabric.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@arch-inc/fabricjs-psbrush@0.0.12/dist/index.js"></script>
+    <script src="/index.js"></script>
+  </>);
+}
 
 export default Index;
