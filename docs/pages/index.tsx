883aba72842ba91dec32177184da01ff73079ad2
Jun Kato <i@junkato.jp>
2020-03-29 19:17:59 +0900

    Update top page

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -34,14 +34,16 @@ const Index: NextPage = () => {
         background: #fff;
         padding: 2em 0 0 0;
       }
-      div.canvas {
+      div.canvas-wrapper {
         background: #f5f5f5;
         margin: 2em auto 0 auto;
         padding: 2em 0;
-        text-align: center;
-        overflow-x: hidden;
       }
-      div.canvas :global(.canvas-container) {
+      div.canvas-wrapper > div.canvas {
+        text-align: center;
+        overflow: hidden;
+      }
+      div.canvas-wrapper > div.canvas :global(.canvas-container) {
         margin: auto;
         border: 1px solid #eee;
         background: #fff;
@@ -53,16 +55,61 @@ const Index: NextPage = () => {
     <div className="hero">
       <div className="ui container">
         <h1 className="ui header">
-          fabricjs-psbrush
+          <div className="content">
+            fabricjs-psbrush
+            <div className="sub header">A lightweight pressure-sensitive brush implementation for Fabric.js</div>
+          </div>
         </h1>
       </div>
     </div>
     <div className="main content">
       <div className="ui container">
-        <p>We're working on documentations -- stay tuned!</p>
-        <p>For now, please refer to <a href="https://arch-inc.github.io/fabricjs-psbrush/index.js">the code</a> to see the minimal working example of <code>PSBrush</code>.</p>
+        <div className="ui message">
+          <div className="header">How to install</div>
+          <pre>npm i @arch-inc/fabricjs-psbrush</pre>
+          <p>For more details on how to use this library, please refer to the following documents.</p>
+        </div>
+        <div className="ui selection divided list">
+          <a className="item" href="https://www.npmjs.com/package/@arch-inc/fabricjs-psbrush">
+            <i className="npm icon"></i>
+            <div className="content">
+              <div className="header">NPM package registry</div>
+              <div className="description">@arch-inc/fabricjs-psbrush</div>
+            </div>
+          </a>
+          <a className="item" href="https://github.com/arch-inc/fabricjs-psbrush">
+            <i className="github icon"></i>
+            <div className="content">
+              <div className="header">GitHub repository</div>
+              <div className="description">@arch-inc/fabricjs-psbrush</div>
+            </div>
+          </a>
+          <a className="item" href="https://arch-inc.github.io/fabricjs-psbrush/api/globals.html">
+            <i className="file alternate outline icon"></i>
+            <div className="content">
+              <div className="header">API document</div>
+              <div className="description">automatically generated with TypeDoc</div>
+            </div>
+          </a>
+          <a className="item" href="https://arch-inc.github.io/fabricjs-psbrush/index.js">
+            <i className="file code outline icon"></i>
+            <div className="content">
+              <div className="header">index.js</div>
+              <div className="description">example code loaded in this page</div>
+            </div>
+          </a>
+        </div>
       </div>
-      <div className="canvas">{canvas}</div>
+      <div className="canvas-wrapper">
+        <div className="ui container">
+          <h3 className="ui header">Live demo</h3>
+          <p>Draw anything on the white blank canvas shown below; tested on iPad Pro and Surface Go.</p>
+          <div className="ui divider"></div>
+        </div>
+        <div className="canvas">
+          {canvas}
+        </div>
+      </div>
     </div>
     <footer>
       <div className="ui container">
@@ -77,7 +124,7 @@ const Index: NextPage = () => {
       </div>
     </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/3.6.2/fabric.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@arch-inc/fabricjs-psbrush@0.0.12/dist/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@arch-inc/fabricjs-psbrush@0.0.15/dist/index.js"></script>
     <script src={`${basePath}/index.js`}></script>
   </>);
 }
