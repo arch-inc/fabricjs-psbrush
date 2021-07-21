3547bc2fd0a25baecb865411bf8f29f9d19b949b
Mark Howard <mark@clearfield.com>
2021-07-21 17:39:06 +1200

    Added prepare script Remove dist folder from git`

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import PSBrush, { PSBrushIface } from "./PSBrush";
-import PSPoint from "./PSPoint";
-import PSStroke, { PSStrokeIface } from "./PSStroke";
-import Simplify from "./Simplify";
-export * from "./utils";
-export { PSBrush, PSBrushIface, PSStroke, PSStrokeIface, PSPoint, Simplify };
