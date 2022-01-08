cb4fd76bd0e05ea438b6f240cee5ed54b759ce21
arcatdmz <arcatdmz@users.noreply.github.com>
2022-01-08 17:43:11 +0000

    deploy: dbd26c35a62ab146c5e8944005eea59b728bd194

---
+++ _next/static/AihhHrVz1UJXdtKwylcr5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-20499f77fe3eefc8.js"],"/_error":["static/chunks/pages/_error-13fe72b639248d20.js"],"/import":["static/chunks/pages/import-ba27ad9d5ddc915f.js"],sortedPages:["/","/_app","/_error","/import"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
